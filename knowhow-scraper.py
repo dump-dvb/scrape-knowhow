5e0c1ca61d9fa4c94f0f8544b2c017eaa364ea8e
Grigory Shipunov <blame@oxapentane.com>
2022-04-23 16:20:44 +0200

    try to sanitize input and some small cleanups

--- knowhow-scraper.py
+++ knowhow-scraper.py
@@ -6,39 +6,45 @@ import time
 
 pdf_base_url =  r'https://knowhow.vdv.de/wp-admin/admin-post.php?action=serve_pdf&token='
 token_base_url =  r'https://knowhow.vdv.de/wp-json/kh-tm/v1/document/'
-variant = [r'?variant=lang-de_fulltext'   r'' r'?variant=lang-en_fulltext'  ]
+variant = [  r'variant=lang-de_fulltext', r'variant=lang-en_fulltext' ]
 outdir = r'./out/'
 
 with open('documents.json', 'r') as f:
     documents = json.load(f)
 
-for d in documents:
-    for e in variant:
-        r = requests.get(token_base_url + str(d['id']) + e)
+for e in variant:
+    for d in documents:
+        token_url = token_base_url + str(d['id']) + r'?' + e
+        sys.stderr.write("ID {0}: Trying token at {1}\n".format(d['id'], token_url))
+        r = requests.get(token_url)
         if r.ok:
             token = json.loads(r.content)['token']
-            sys.stderr.write("Id {0}: got token {1}\n".format(d['id'], token))
+            sys.stderr.write("ID {0}: got token {1}\n".format(d['id'], token))
             r.close()
         else:
-            sys.stderr.write("Id {0}: HTTP {1}\n".format(d['id'], r.status_code))
+            sys.stderr.write("ID {0}: HTTP {1}\n".format(d['id'], r.status_code))
             r.close()
             pass
 
         rpdf = requests.get(pdf_base_url + token)
-        sys.stderr.write("Id {0}: HTTP {1}\n".format(d['id'], rpdf.status_code))
+        sys.stderr.write("ID {0}: HTTP {1}\n".format(d['id'], rpdf.status_code))
 
-        if 'application/octet-stream' in rpdf.headers.get('content-type'):
-            sys.stderr.write("Id {0}: Got octet-stream ^_^\n".format(d['id']))
-            if rpdf.content[0:5] == b"%PDF-":
-                filename = outdir + d['doc_num'] + '_' + str(d['id']) + '_' +  d['title@de_DE_formal'] + '.pdf'
-                with open(filename, 'wb') as outpdf:
-                    outpdf.write(rpdf.content)
-                    sys.stderr.write("Id {0}: wrote {1}\n".format(d['id'], rpdf.status_code))
+        try:
+
+            if 'application/octet-stream' in rpdf.headers.get('content-type'):
+                sys.stderr.write("ID {0}: Got octet-stream ^_^\n".format(d['id']))
+                if rpdf.content[1:4] == b"PDF":
+                    filename = d['doc_num'] + '_' + str(d['id']) + '_' +  d['title@de_DE_formal'][0:20] + e +  '.pdf'
+                    filename = filename.replace(r'/', '_').replace(r'=', '-')
+                    with open(outdir + filename, 'wb') as outpdf:
+                        outpdf.write(rpdf.content)
+                        sys.stderr.write("ID {0}: wrote {1}\n".format(d['id'], outdir + filename))
+                else:
+                    sys.stderr.write("ID {0}: got {1} content type but no PDF\n".format(d['id'], rpdf.headers.get('content-type')))
             else:
-                sys.stderr.write("id {0}: got {1} content type but no PDF\n".format(d['id'], rpdf.headers.get('content-type')))
-        else:
-            sys.stderr.write("id {0}: got {1} content type\n".format(d['id'], rpdf.headers.get('content-type')))
-            #print(rpdf.content)
- 
-        rpdf.close()
+                sys.stderr.write("ID {0}: got {1} content type\n".format(d['id'], rpdf.headers.get('content-type')))
+     
+            rpdf.close()
+        except FileNotFoundError:
+            pass
 
